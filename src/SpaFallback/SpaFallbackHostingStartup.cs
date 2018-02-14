41f51bfe1674452a18e1f96881b972dc2d9040d9
Kristian Hellang <kristian.hellang@iris.no>
2018-02-14 16:54:55 +0100

    Add hosting startup to SpaFallback

---
+++ src/SpaFallback/SpaFallbackHostingStartup.cs
@@ -0,0 +1,15 @@
+﻿using Hellang.Middleware.SpaFallback;
+using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(SpaFallbackHostingStartup))]
+
+namespace Hellang.Middleware.SpaFallback
+{
+    public class SpaFallbackHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => services.AddSpaFallback());
+        }
+    }
+}
