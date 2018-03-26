b2d5be1ffbd1f818cd79266e67fc365e9d813e2d
Kristian Hellang <kristian@hellang.com>
2018-03-27 00:53:15 +0200

    More complete set of claim types

--- src/Authentication.JwtBearer.Google/GoogleClaimTypes.cs
+++ src/Authentication.JwtBearer.Google/GoogleClaimTypes.cs
@@ -4,16 +4,30 @@ namespace Hellang.Authentication.JwtBearer.Google
 {
     public static class GoogleClaimTypes
     {
-        public static string Name { get; } = "name";
-
         public static string Domain { get; } = "hd";
+
+        public static string Name { get; } = "name";
 
         public static string Locale { get; } = "locale";
 
         public static string Picture { get; } = "picture";
 
-        public static string EmailVerified { get; } = "email_verified";
+        public static string Profile { get; } = "profile";
 
+        public static string EmailVerified { get; } = "email_verified";
+        
+        public static string Iss { get; } = JwtRegisteredClaimNames.Iss;
+
+        public static string Sub { get; } = JwtRegisteredClaimNames.Sub;
+
+        public static string Azp { get; } = JwtRegisteredClaimNames.Azp;
+
+        public static string Aud { get; } = JwtRegisteredClaimNames.Aud;
+
+        public static string Iat { get; } = JwtRegisteredClaimNames.Iat;
+
+        public static string Exp { get; } = JwtRegisteredClaimNames.Exp;
+        
         public static string Email { get; } = JwtRegisteredClaimNames.Email;
 
         public static string GivenName { get; } = JwtRegisteredClaimNames.GivenName;
