4a67e2c65212bd31e66a1e31c889ce4e3e4d530d
Maxime Beaudry <mabead@users.noreply.github.com>
2019-06-17 09:35:55 -0400

    Include details in ProblemDetailsException
    See https://github.com/khellang/Middleware/issues/57 for more details.

--- src/ProblemDetails/ProblemDetailsException.cs
+++ src/ProblemDetails/ProblemDetailsException.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Text;
 
 namespace Hellang.Middleware.ProblemDetails
 {
     public class ProblemDetailsException : Exception
     {
         public ProblemDetailsException(Microsoft.AspNetCore.Mvc.ProblemDetails details)
+            :base($"{details.Type} : {details.Title}")
         {
             Details = details;
         }
 
         public Microsoft.AspNetCore.Mvc.ProblemDetails Details { get; }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Type    : {Details.Type}");
+            stringBuilder.AppendLine($"Title   : {Details.Title}");
+            stringBuilder.AppendLine($"Status  : {Details.Status}");
+            stringBuilder.AppendLine($"Detail  : {Details.Detail}");
+            stringBuilder.AppendLine($"Instance: {Details.Instance}");
+
+            return stringBuilder.ToString();
+        }
     }
 }
