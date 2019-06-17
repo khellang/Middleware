54b302c6f2f17598a69ab476d9a04ac637f56cae
Maxime Beaudry <mabead@users.noreply.github.com>
2019-06-17 09:35:55 -0400

    Include details in ProblemDetailsException
    See https://github.com/khellang/Middleware/issues/57 for more details.

---
+++ test/ProblemDetails.Tests/ProblemDetailsExceptionTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Hellang.Middleware.ProblemDetails.Tests
+{
+    public class ProblemDetailsExceptionTests
+    {
+        private static Microsoft.AspNetCore.Mvc.ProblemDetails CreateProblemDetails()
+        {
+            return new Microsoft.AspNetCore.Mvc.ProblemDetails
+            {
+                Type = "https://httpstatuses.com/303",
+                Title = "See other",
+                Status = 303,
+                Detail = "Look somewhere else.",
+                Instance = "https://example.com/problem/123",
+            };
+
+        }
+
+        [Fact]
+        public void Constructor_InitializesMessage()
+        {
+            var problemDetails = CreateProblemDetails();
+
+            var exception = new ProblemDetailsException(problemDetails);
+
+            Assert.Equal("https://httpstatuses.com/303 : See other", exception.Message);
+        }
+
+        [Fact]
+        public void ToString_ReturnsAllDetails()
+        {
+            var problemDetails = CreateProblemDetails();
+
+            var exception = new ProblemDetailsException(problemDetails);
+            var actual = exception.ToString();
+
+            var expected = @"Type    : https://httpstatuses.com/303
+Title   : See other
+Status  : 303
+Detail  : Look somewhere else.
+Instance: https://example.com/problem/123
+";
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
