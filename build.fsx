b78dd411b33f528f63698e009f763aaaea25a2fa
David Cumps <david@cumps.be>
2019-12-15 19:34:07 +0100

    feat: upgrade to netcoreapp31

--- build.fsx
+++ build.fsx
@@ -1,8 +1,8 @@
 #r "paket:
-version 5.226.0
+version 5.241.2
 framework: netstandard20
 source https://api.nuget.org/v3/index.json
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 3.0.1 //"
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 3.2.0 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
