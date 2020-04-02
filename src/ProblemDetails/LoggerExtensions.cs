2dfb4e570b7a0ddac1963ea4ddf1cabff55fa25f
T.M <tomasz.piotr.malinowski@gmail.com>
2020-04-02 09:33:28 +0200

    fixing EventId

--- src/ProblemDetails/LoggerExtensions.cs
+++ src/ProblemDetails/LoggerExtensions.cs
@@ -8,14 +8,14 @@ namespace Hellang.Middleware.ProblemDetails
         private static readonly Action<ILogger, Exception> _unhandledException =
             LoggerMessage.Define(LogLevel.Error, new EventId(1, "UnhandledException"), "An unhandled exception has occurred while executing the request.");
 
-        private static readonly Action<ILogger, Exception> _ignoredException =
-            LoggerMessage.Define(LogLevel.Information, new EventId(1, "IgnoredException"), "An exception has occurred while executing the request, but it was ignored by custom mapping rules.");
-
         private static readonly Action<ILogger, Exception> _responseStarted =
             LoggerMessage.Define(LogLevel.Warning, new EventId(2, "ResponseStarted"), "The response has already started, the problem details middleware will not be executed.");
 
         private static readonly Action<ILogger, Exception> _problemDetailsMiddlewareException =
             LoggerMessage.Define(LogLevel.Error, new EventId(3, "Exception"), "An exception was thrown attempting to execute the problem details middleware.");
+
+        private static readonly Action<ILogger, Exception> _ignoredException =
+            LoggerMessage.Define(LogLevel.Information, new EventId(4, "IgnoredException"), "An exception has occurred while executing the request, but it was ignored by custom mapping rules.");
 
         public static void UnhandledException(this ILogger logger, Exception exception)
         {
