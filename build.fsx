181c0121cba3e6a34d5c6831bf071d946fd4e856
David Cumps <david@cumps.be>
2020-09-21 12:30:22 +0200

    fix: move to 3.1.8

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 version 5.241.6
 framework: netstandard20
 source https://api.nuget.org/v3/index.json
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 4.2.2 //"
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 4.2.3 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
