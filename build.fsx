54898d6b44cb044f6a65c2b0b8ba0301c0fb8872
David Cumps <david@cumps.be>
2020-12-17 18:26:00 +0100

    fix: move to 5.0.1

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 version 6.0.0-beta8
 framework: netstandard20
 source https://api.nuget.org/v3/index.json
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 4.2.5 //"
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 5.0.1 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
@@ -14,20 +14,26 @@ open ``Build-generic``
 let assemblyVersionNumber = (sprintf "%s.0")
 let nugetVersionNumber = (sprintf "%s")
 
-let build = buildSolution assemblyVersionNumber
-let publish = publishSolution assemblyVersionNumber
+let buildSource = build assemblyVersionNumber
+let buildTest = buildTest assemblyVersionNumber
+let publishSource = publish assemblyVersionNumber
 let pack = packSolution nugetVersionNumber
 
 supportedRuntimeIdentifiers <- [ "linux-x64" ]
 
 // Library ------------------------------------------------------------------------
-Target.create "Lib_Build" (fun _ -> build "Be.Vlaanderen.Basisregisters.ProblemDetails")
+Target.create "Lib_Build" (fun _ ->
+  buildSource "Be.Vlaanderen.Basisregisters.ProblemDetails"
+  buildTest "Be.Vlaanderen.Basisregisters.ProblemDetails.Tests"
+)
+
 Target.create "Lib_Test" (fun _ ->
   [
     "test" @@ "Be.Vlaanderen.Basisregisters.ProblemDetails.Tests" ]
   |> List.iter testWithDotNet
 )
-Target.create "Lib_Publish" (fun _ -> publish "Be.Vlaanderen.Basisregisters.ProblemDetails")
+
+Target.create "Lib_Publish" (fun _ -> publishSource "Be.Vlaanderen.Basisregisters.ProblemDetails")
 Target.create "Lib_Pack" (fun _ -> pack "Be.Vlaanderen.Basisregisters.ProblemDetails")
 
 // --------------------------------------------------------------------------------
