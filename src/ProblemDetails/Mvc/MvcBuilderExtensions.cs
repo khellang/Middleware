65ec7600859ac4dcd4e5d11a5f5482719f3a93ab
lonix1 <40320097+lonix1@users.noreply.github.com>
2021-10-18 09:25:17 +0200

    Added AddProblemDetailsConventions overload (#1)

--- src/ProblemDetails/Mvc/MvcBuilderExtensions.cs
+++ src/ProblemDetails/Mvc/MvcBuilderExtensions.cs
@@ -19,17 +19,32 @@ namespace Hellang.Middleware.ProblemDetails.Mvc
         /// <returns>The <see cref="IMvcBuilder"/>.</returns>
         public static IMvcBuilder AddProblemDetailsConventions(this IMvcBuilder builder)
         {
-            // Forward the MVC problem details factory registration to the factory used by the middleware.
-            builder.Services.Replace(
-                ServiceDescriptor.Singleton<MvcProblemDetailsFactory>(p => p.GetRequiredService<ProblemDetailsFactory>()));
-
-            builder.Services.TryAddEnumerable(
-                ServiceDescriptor.Transient<IConfigureOptions<ApiBehaviorOptions>, ProblemDetailsApiBehaviorOptionsSetup>());
-
-            builder.Services.TryAddEnumerable(
-                ServiceDescriptor.Transient<IApplicationModelProvider, ProblemDetailsApplicationModelProvider>());
+            AddProblemDetailsConventions(builder.Services);
 
             return builder;
+        }
+
+        /// <summary>
+        /// Adds conventions to turn off MVC's built-in <see cref="ApiBehaviorOptions.ClientErrorMapping"/>,
+        /// adds a <see cref="ProducesErrorResponseTypeAttribute"/> to all actions with in controllers with an
+        /// <see cref="ApiControllerAttribute"/> and a result filter that transforms <see cref="ObjectResult"/>
+        /// containing a <see cref="string"/> to <see cref="ProblemDetails"/> responses.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddProblemDetailsConventions(this IServiceCollection services)
+        {
+            // Forward the MVC problem details factory registration to the factory used by the middleware.
+            services.Replace(
+                ServiceDescriptor.Singleton<MvcProblemDetailsFactory>(p => p.GetRequiredService<ProblemDetailsFactory>()));
+
+            services.TryAddEnumerable(
+                ServiceDescriptor.Transient<IConfigureOptions<ApiBehaviorOptions>, ProblemDetailsApiBehaviorOptionsSetup>());
+
+            services.TryAddEnumerable(
+                ServiceDescriptor.Transient<IApplicationModelProvider, ProblemDetailsApplicationModelProvider>());
+
+            return services;
         }
     }
 }
