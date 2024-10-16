844ad93472b151ed2db23280816b2411c24254f3
Arman Ossi Loko <armanossiloko@outlook.com>
2024-10-17 00:18:30 +0200

    Removed netcoreapp3.1 and added net8.0 support

--- samples/ProblemDetails.MinimalApiSample/Program.cs
+++ samples/ProblemDetails.MinimalApiSample/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ samples/ProblemDetails.Mvc.Sample/ProblemDetailsOptionsExtensions.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using Hellang.Middleware.ProblemDetails;
+
+namespace ProblemDetails.Mvc.Sample;
+
+public static class ProblemDetailsOptionsExtensions
+{
+    public static void MapFluentValidationException(this ProblemDetailsOptions options) =>
+        options.Map<ValidationException>((ctx, ex) =>
+        {
+            var factory = ctx.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+
+            var errors = ex.Errors
+                .GroupBy(x => x.PropertyName)
+                .ToDictionary(
+                    x => x.Key,
+                    x => x.Select(x => x.ErrorMessage).ToArray());
+
+            return factory.CreateValidationProblemDetails(ctx, errors);
+        });
+}
